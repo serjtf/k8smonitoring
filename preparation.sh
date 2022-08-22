05b3aeff2d42d5f35d0ec56d4f4b7d2739fd1952
serjtf <107878358+serjtf@users.noreply.github.com>
2022-08-22 18:25:30 +0100

    added helm installation

--- preparation.sh
+++ preparation.sh
@@ -68,3 +68,7 @@ echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
 sudo mv -v /tmp/eksctl /usr/local/bin
+
+# install helm
+
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
